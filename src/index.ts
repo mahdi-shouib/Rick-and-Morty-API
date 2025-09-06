e0e2781976cfc619442d8f870d784d43a84705c5
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-06 20:24:09 +0300

    Add simple api to return relative character if exists

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@ import fs from 'fs/promises';
 const app = express();
 const port = 5000;
 
-app.get('/api/character', async (req, res) => {
-    const c = await fs.readFile('src/characters.json', 'utf-8');
-    res.json(JSON.parse(c));
-})
+app.get('/api/character/:id', async (req, res) => {
+    const c = JSON.parse(await fs.readFile('src/characters.json', 'utf-8'));
+    const id = parseInt(req.params.id);
+
+    c.result.forEach((character: any, index: number) => {
+        if (id === character.id) {
+            res.json(c.result[index]);
+            return;
+        }
+    });
+
+    res.json(c);
+});
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
