88d2c5815fc95bae1879a68facb20194cac79735
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-06 20:32:06 +0300

    Fix bug that tried to response multiple times

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,17 @@ const port = 5000;
 app.get('/api/character/:id', async (req, res) => {
     const c = JSON.parse(await fs.readFile('src/characters.json', 'utf-8'));
     const id = parseInt(req.params.id);
+    let found = false;
 
     c.result.forEach((character: any, index: number) => {
         if (id === character.id) {
             res.json(c.result[index]);
+            found = true;
             return;
         }
     });
 
-    res.json(c);
+    if (!found) res.status(404).send('Character not found!');
 });
 
 app.get('/api/character', async (req, res) => {
